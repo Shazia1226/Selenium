c94170d0dde8577f7f9563f6665f93ca1accb8d1
Shazia1226 <63747134+Shazia1226@users.noreply.github.com>
2020-05-01 18:54:51 -0700

    Ravi sir

---
+++ Project2/src/Pack3/Class4.java
@@ -0,0 +1,20 @@
+package Pack3;
+
+import Pack1.Class1;
+
+public class Class4 extends Class1
+{
+
+	public Class4(int a, int b) 
+	{
+		super(a, b);
+		
+	}
+
+	public static void main(String[] args)
+	{
+		Class4 c4= new Class4(2000,4000);
+		c4.add();
+	}
+
+}
